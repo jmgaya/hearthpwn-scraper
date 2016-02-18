7e24fc922e47219ad06d96370a320fcc22ac2f4f
jmunoz <jmunoz@tuenti.com>
2016-02-18 22:31:26 +0100

    Modified CS following Airbnb

--- src/hearthpwn-scraper.js
+++ src/hearthpwn-scraper.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const cheerio = require('cheerio');
 const EventEmitter = require('events').EventEmitter;
 const req = require('request');
@@ -7,48 +5,47 @@ const iconv = require('iconv');
 
 const MAIN_URL = 'http://www.hearthpwn.com';
 const FILTER_OPTIONS =
-{
-  druid: 4,
-  hunter: 8,
-  mage: 16,
-  paladin: 32,
-  priest: 64,
-  rogue: 128,
-  shaman: 256,
-  warlock: 512,
-  warrior: 1024,
-}
+  {
+    druid: 4,
+    hunter: 8,
+    mage: 16,
+    paladin: 32,
+    priest: 64,
+    rogue: 128,
+    shaman: 256,
+    warlock: 512,
+    warrior: 1024,
+  };
 
 const request = (options, callback) => {
-    req(options, function(err, resp, body) {
-        var ic = new iconv.Iconv('iso-8859-1', 'utf-8');
-        var buf = ic.convert(body);
-        var utf8Body = buf.toString('utf-8');
-        callback(err, resp, utf8Body);
-    });
+  req(options, (err, resp, body) => {
+    const ic = new iconv.Iconv('iso-8859-1', 'utf-8');
+    const buf = ic.convert(body);
+    const utf8Body = buf.toString('utf-8');
+    callback(err, resp, utf8Body);
+  });
 };
 
-const getFilter = options => {
-  return options && options.heroes ? getHeroFilter(options.heroes) : '';
-}
+const getHeroFilterSummatory = heroes =>
+  heroes.reduce((ac, hero) => ac + (FILTER_OPTIONS[hero.toLowerCase()] | 0), 0);
 
-const getHeroFilter = heroes => {
-  return '?filter-class=' + heroes.reduce((ac, hero) => ac + (FILTER_OPTIONS[hero.toLowerCase()] | 0), 0);
-}
+const getHeroFilter = heroes =>
+  `?filter-class=${getHeroFilterSummatory(heroes)}`;
 
-const parseDeck = elem => {
-  return {
+const getFilter = options =>
+  options && options.heroes ? getHeroFilter(options.heroes) : '';
+
+const parseDeck = elem =>
+  Object.freeze({
     title: elem.children[0].data,
-    url: MAIN_URL+elem.attribs.href
-  }
-}
+    url: MAIN_URL + elem.attribs.href,
+  });
 
-const parseCard = elem => {
-  return {
+const parseCard = elem =>
+  Object.freeze({
     card: elem.children[0].data.trim(),
-    quantity: parseInt(elem.parent.next.data.trim().split(' ')[1])
-  }
-}
+    quantity: parseInt(elem.parent.next.data.trim().split(' ')[1], 10),
+  });
 
 /**
  * Returns an array containing the first page of the most popular decks
@@ -56,13 +53,12 @@ const parseCard = elem => {
  * @returns {EventEmitter}
  */
 const getPopularDecks = options => {
+  const scraper = new EventEmitter();
+  const popularDecksUrl = { url: `${MAIN_URL}/decks${getFilter(options)}` };
 
-  const scraper = new EventEmitter(),
-        popularDecksUrl = {'url': MAIN_URL+'/decks'+getFilter(options)};
-
-  request(popularDecksUrl, function(err, resp, body) {
+  request(popularDecksUrl, (err, resp, body) => {
     const $ = cheerio.load(body);
-    let popularDecks = [];
+    const popularDecks = [];
     $('tbody')
       .find('a')
       .filter((i) => i % 2 === 0)
@@ -71,7 +67,7 @@ const getPopularDecks = options => {
   });
 
   return scraper;
-}
+};
 
 /**
  * Returns an array containing every card included in the given deck url
@@ -79,13 +75,12 @@ const getPopularDecks = options => {
  * @returns {EventEmitter}
  */
 const getDeckInfo = url => {
+  const scraper = new EventEmitter();
+  const deckInfoUrl = { url };
 
-  const scraper = new EventEmitter(),
-        deckInfoUrl = {'url': url};
-
-  request(deckInfoUrl, function(err, resp, body) {
+  request(deckInfoUrl, (err, resp, body) => {
     const $ = cheerio.load(body);
-    let deckInfo = [];
+    const deckInfo = [];
     $('aside')
       .find('tbody td b a')
       .each((i, elem) => deckInfo.push(parseCard(elem)));
@@ -93,7 +88,7 @@ const getDeckInfo = url => {
   });
 
   return scraper;
-}
+};
 
 exports.getPopularDecks = getPopularDecks;
 exports.getDeckInfo = getDeckInfo;
